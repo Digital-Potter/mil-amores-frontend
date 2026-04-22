406d07dfbd659509ea77a53b7a14d9df79ef55a4
Norman C. Pleitez <webmaster@plitzcorporation.com>
2026-04-22 15:26:02 -0400

    [* main] specials show only published, not drafts

--- components/SpecialsCarousel/CarouselSetup.tsx
+++ components/SpecialsCarousel/CarouselSetup.tsx
@@ -39,88 +39,93 @@ const CarouselSetup = ({ specials }: CarouselSetupProps) => {
 			<div className="dp-container grid grid-cols-6 lg:grid-cols-12">
 				{specials
 					.sort((a, b) => a.position - b.position)
-					.map((special, index) => (
-						<div
-							key={special._id}
-							className={twJoin(
-								'col-start-1 col-end-7 row-start-1 transition-opacity duration-1000 lg:col-end-13',
-								index === currentSlide && 'z-10 opacity-100',
-								index !== currentSlide && 'z-0 opacity-0',
-							)}
-						>
-							<div className="grid grid-cols-6 gap-9 lg:grid-cols-12">
-								<div className="order-1 col-span-6 flex h-full flex-col justify-between lg:col-span-4">
-									<div className="text-dp-ma-orange mb-10 flex flex-row items-center justify-start gap-3">
-										<HotPepper className="h-11 w-10 lg:h-16 lg:w-16" />
-										<span className="font-Croissant ml-2 text-xl lg:text-3xl">
-											Spicy Stuff
-										</span>
-									</div>
-									<div className="mb-10 flex flex-col">
-										<h3 className="text-4xl font-bold text-balance lg:text-7xl">
-											{special.title}
-										</h3>
-									</div>
-									<div className="z-20 flex flex-row items-center gap-3.5">
-										{specials.map((_, dotIndex) => (
-											<button
-												key={dotIndex}
-												className={twMerge(
-													'cursor-pointer rounded-full',
-													dotIndex === currentSlide
-														? 'bg-dp-highlighter-ma-green h-5 w-5'
-														: 'h-3 w-3 bg-gray-300',
-												)}
-												onClick={() => setCurrentSlide(dotIndex)}
-											/>
-										))}
-									</div>
-								</div>
-								<div className="order-3 col-span-6 lg:order-2 lg:col-span-4">
-									{special.featuredImage && (
-										<DecoratedImage
-											imageSrc={`${special.featuredImage.fullSize}`}
-											imageData={{
-												alt: createImageAlt(special.featuredImage.fullSize),
-												width: 500,
-												height: 448,
-											}}
-											decorationConfig={imageDecorationConfig}
-											className="top-10 -mb-10 h-80 lg:-mt-32 lg:mb-0 xl:-top-10 xl:h-112"
-										/>
+					.map(
+						(special, index) =>
+							special.isLive && (
+								<div
+									key={special._id}
+									className={twJoin(
+										'col-start-1 col-end-7 row-start-1 transition-opacity duration-1000 lg:col-end-13',
+										index === currentSlide && 'z-10 opacity-100',
+										index !== currentSlide && 'z-0 opacity-0',
 									)}
-								</div>
-								<div className="order-2 col-span-6 flex flex-col justify-between lg:order-3 lg:col-span-4">
-									<div className="flex flex-col gap-7 lg:ml-10">
-										<h4 className="text-2xl text-balance lg:text-3xl">
-											{special.subtitle}
-										</h4>
-
-										<div
-											className={twMerge(
-												'prose text-dp-dark-ma-brown lg:prose-lg max-w-none text-base font-bold text-balance [&>p]:lg:text-2xl',
+								>
+									<div className="grid grid-cols-6 gap-9 lg:grid-cols-12">
+										<div className="order-1 col-span-6 flex h-full flex-col justify-between lg:col-span-4">
+											<div className="text-dp-ma-orange mb-10 flex flex-row items-center justify-start gap-3">
+												<HotPepper className="h-11 w-10 lg:h-16 lg:w-16" />
+												<span className="font-Croissant ml-2 text-xl lg:text-3xl">
+													Spicy Stuff
+												</span>
+											</div>
+											<div className="mb-10 flex flex-col">
+												<h3 className="text-4xl font-bold text-balance lg:text-7xl">
+													{special.title}
+												</h3>
+											</div>
+											<div className="z-20 flex flex-row items-center gap-3.5">
+												{specials.map((_, dotIndex) => (
+													<button
+														key={dotIndex}
+														className={twMerge(
+															'cursor-pointer rounded-full',
+															dotIndex === currentSlide
+																? 'bg-dp-highlighter-ma-green h-5 w-5'
+																: 'h-3 w-3 bg-gray-300',
+														)}
+														onClick={() => setCurrentSlide(dotIndex)}
+													/>
+												))}
+											</div>
+										</div>
+										<div className="order-3 col-span-6 lg:order-2 lg:col-span-4">
+											{special.featuredImage && (
+												<DecoratedImage
+													imageSrc={`${special.featuredImage.fullSize}`}
+													imageData={{
+														alt: createImageAlt(special.featuredImage.fullSize),
+														width: 500,
+														height: 448,
+													}}
+													decorationConfig={imageDecorationConfig}
+													className="top-10 -mb-10 h-80 lg:-mt-32 lg:mb-0 xl:-top-10 xl:h-112"
+												/>
 											)}
-											dangerouslySetInnerHTML={{ __html: special.description }}
-										/>
+										</div>
+										<div className="order-2 col-span-6 flex flex-col justify-between lg:order-3 lg:col-span-4">
+											<div className="flex flex-col gap-7 lg:ml-10">
+												<h4 className="text-2xl text-balance lg:text-3xl">
+													{special.subtitle}
+												</h4>
 
-										{special.discountType === '%' && (
-											<span className="text-dp-ma-orange text-5xl font-bold">
-												{special.discount}% OFF
+												<div
+													className={twMerge(
+														'prose text-dp-dark-ma-brown lg:prose-lg max-w-none text-base font-bold text-balance [&>p]:lg:text-2xl',
+													)}
+													dangerouslySetInnerHTML={{
+														__html: special.description,
+													}}
+												/>
+
+												{special.discountType === '%' && (
+													<span className="text-dp-ma-orange text-5xl font-bold">
+														{special.discount}% OFF
+													</span>
+												)}
+												{special.discountType === '$' && (
+													<span className="text-dp-ma-orange text-5xl font-bold">
+														Only ${special.discount}
+													</span>
+												)}
+											</div>
+											<span className="mt-4 text-xs lg:ml-10 lg:text-sm">
+												{special.disclaimer}
 											</span>
-										)}
-										{special.discountType === '$' && (
-											<span className="text-dp-ma-orange text-5xl font-bold">
-												Only ${special.discount}
-											</span>
-										)}
+										</div>
 									</div>
-									<span className="mt-4 text-xs lg:ml-10 lg:text-sm">
-										{special.disclaimer}
-									</span>
 								</div>
-							</div>
-						</div>
-					))}
+							),
+					)}
 			</div>
 		</section>
 	);
